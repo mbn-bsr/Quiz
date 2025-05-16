ba41197ec799e2085bf21ab5016ea042ea9d651f
Mubeen <mubeen.basheer@gmail.com>
2025-05-16 15:31:57 +0100

    committing everything so far to push to github

--- script.js
+++ script.js
@@ -12,19 +12,25 @@ function newElement(element, parent, elClass) {
   return newL;
 }
 
-function answerArrMixer(obj) {
-  let newR = [...obj.incorrect_answers];
-  newR.push(obj.correct_answer);
-  return newR.sort(() => 0.5 - Math.random());
-}
-
 function btnMaker(text, elementMaker) {
   let btn = elementMaker("button", wrapper, "btn");
   btn.innerHTML = text;
   return btn;
 }
 
+function answerArrMixer(obj) {
+  let newR = [...obj.incorrect_answers, obj.correct_answer];
+  //newR.push(obj.correct_answer);
+  let answerArr =  newR.sort(() => 0.5 - Math.random());
+  //let checkBox = elementMaker()
+  return answerArr;
+  //⏩⏩⏩PROBABLY CREATE A FUNCTION THAT INSERTS ANSWER ARRAY'S INDIVIDUAL ELEMENTS INTO SEPARATE RADIO BUTTONS/CHECKBOXES
+}
+
+
+
 var wrapper = newElement("div", document.body, "wrapper");
+let bgImg = newElement('div', wrapper, 'bgImg');
 let nextBtn = btnMaker("Next", newElement);
 nextBtn.style.display = "none";
 let prevBtn = btnMaker("Previous", newElement);
@@ -41,69 +47,48 @@ function welcomeAnd1by1Qs(elementMaker, strtBtn) {
   strt.addEventListener("click", () => {
     qNav(welcm, strt);
   });
-  
 }
 
 let currentIndex = 0;
 
-function qNav(w, s) {
-  w.style.display = "none";
-  s.style.display = "none";
-  let Qs = document.getElementsByClassName("question");
-  let Ops = document.getElementsByClassName("options");
-
- 
-  insideNextQ(Qs, Ops);
+function qNav(wlcm, strt) {
+  wlcm.style.display = "none";
+  strt.style.display = "none";
+  let qs = document.getElementsByClassName("question");
+  let ops = document.getElementsByClassName("options");
+  qs[currentIndex].style.display = 'block';
+  ops[currentIndex].style.display = 'block'; 
   nextBtn.style.display = "block";
-
+  nextBtn.addEventListener('click', function nextQ(){insideNextQ(qs, ops)});
+  prevBtn.addEventListener('click', function prevQ(){insidePrevQ(qs, ops)});
 }
 
 
-//NEED TO DESIGN THE NEXT AND PREV FUNCIONS
-function insideNextQ(Qs, Ops) {
 
-  if (currentIndex > 0 && currentIndex <= 9) {
-    Qs[currentIndex - 1].style.display = "none";
-    Ops[currentIndex - 1].style.display = "none";
-    prevBtn.style.display = "block";
-  } 
-    Qs[currentIndex].style.display = "block";
-    Qs[currentIndex].textContent = `Question # ${currentIndex + 1}: ${
-      Qs[currentIndex].textContent
-    }`;
-    Ops[currentIndex].style.display = "block";
-    currentIndex += 1;
-    console.log(currentIndex);
-    nextBtn.addEventListener("click", function nextQ() {
-      nextBtn.removeEventListener('click', nextQ);
-      return insideNextQ(Qs, Ops);
-    });
+function insideNextQ(qs, ops){
+ 
+  currentIndex++;
+  currentIndex > 8 ? nextBtn.disabled = true : nextBtn.disabled = false;
+  currentIndex <= 0 ? prevBtn.disabled = true : prevBtn.disabled = false;
+  currentIndex > 0 ? prevBtn.style.display = 'block' : prevBtn.style.display = 'none';
+  qs[currentIndex-1].style.display = 'none';
+  ops[currentIndex-1].style.display = 'none';
+  qs[currentIndex].style.display = 'block';
+  ops[currentIndex].style.display = 'block';
   
 }
 
-function insidePrevQ(Qs, Ops) {
-  if (currentIndex == 0) {
-    Qs[currentIndex + 1].style.display = "none";
-    Ops[currentIndex + 1].style.display = "none";
-    prevBtn.style.display = "none";
-    nextBtn.style.display = "block";
-  }
-    Qs[currentIndex].style.display = "block";
-    Qs[currentIndex].textContent = `Question # ${currentIndex + 1}: ${
-      Qs[currentIndex].textContent
-    }`;
-    Ops[currentIndex].style.display = "block";
-  
-  currentIndex -= 1;
-  console.log(currentIndex);
+function insidePrevQ(qs, ops){
+  currentIndex--;
+  currentIndex > 8 ? nextBtn.disabled = true : nextBtn.disabled = false;
+  currentIndex <= 0 ? prevBtn.disabled = true : prevBtn.disabled = false;
+  qs[currentIndex+1].style.display = 'none';
+  ops[currentIndex+1].style.display = 'none';
+  qs[currentIndex].style.display = 'block';
+  ops[currentIndex].style.display = 'block';
 }
 
 
-
-function handleNextQ() {
-  nextBtn.removeEventListener("click", handleNextQ);
-  return nextQ(w, s);
-}
 
 async function data() {
   let request = await fetch("https://opentdb.com/api.php?amount=10");
@@ -117,15 +102,17 @@ async function data() {
 data()
   .then((val) => {
     if (val.results) {
-      val.results.forEach((index) =>
-        forDisplay(newElement, answerArrMixer, index)
+      let qNum = 0;
+      val.results.forEach((objOnEachIndx) =>
+        forDisplay(newElement, answerArrMixer, objOnEachIndx)      
       );
 
-      function forDisplay(elementMaker, ansArrMx, ind) {
+      function forDisplay(elementMaker, ansArrMx, indx) {
+        qNum++;
         let question = elementMaker("div", wrapper, "question");
-        question.innerHTML = ind.question;
+        question.innerHTML = `Question #${qNum}: ${indx.question}`;
         let options = elementMaker("div", wrapper, "options");
-        options.innerHTML = ansArrMx(ind);
+        options.innerHTML = ansArrMx(indx);
         question.style.display = "none";
         options.style.display = "none";
       }
